b68dc5ba141d26c59d4bef4716baa99c1cf0782a
Martin Jordán <martin@jordan.cz>
2017-10-03 21:57:12 +0200

    tasks 1 through 5

--- currency-convertor/src/main/java/cz/fi/muni/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/fi/muni/pa165/currency/CurrencyConvertorImpl.java
@@ -2,6 +2,8 @@ package cz.fi.muni.pa165.currency;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -10,6 +12,7 @@ import java.util.Currency;
  * @author petr.adamek@embedit.cz
  */
 public class CurrencyConvertorImpl implements CurrencyConvertor {
+    final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
 
     private final ExchangeRateTable exchangeRateTable;
     //private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
@@ -20,7 +23,20 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (sourceCurrency == null || targetCurrency == null || sourceAmount == null) {
+            throw new IllegalArgumentException("Some argument is not set.");
+        }
+
+        try {
+            BigDecimal exchangeRate = exchangeRateTable.getExchangeRate(sourceCurrency,targetCurrency);
+            if (exchangeRate == null) {
+                throw new UnknownExchangeRateException("You cannot convert these two currencies.");
+            }
+
+            return sourceAmount.multiply(new BigDecimal(exchangeRate.toString()));
+        } catch (ExternalServiceFailureException e) {
+            throw new RuntimeException("Some error in external service.");
+        }
     }
 
 }
--- currency-convertor/src/test/java/cz/fi/muni/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/fi/muni/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,39 +1,58 @@
 package cz.fi.muni.pa165.currency;
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
 
-    @Test
-    public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
-    }
+    @Mock
+    private ExchangeRateTable exchangeRateTable;
+
+    @InjectMocks
+    private CurrencyConvertorImpl currencyConvertor;
 
     @Test
+    public void testConvert() throws ExternalServiceFailureException{
+        when(exchangeRateTable.getExchangeRate(any(Currency.class), any(Currency.class))).thenReturn(new BigDecimal("25"));
+        assertEquals(new BigDecimal("25"),currencyConvertor.convert(Currency.getInstance(Locale.US),Currency.getInstance("CZK"), BigDecimal.ONE));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(null, Currency.getInstance("EUR"), new BigDecimal("10.00"));
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testConvertWithNullTargetCurrency() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(Currency.getInstance("EUR"), null, new BigDecimal("10.00"));
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testConvertWithNullSourceAmount() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(Currency.getInstance("EUR"), Currency.getInstance("EUR"), null);
     }
 
-    @Test
+    @Test(expected = UnknownExchangeRateException.class)
     public void testConvertWithUnknownCurrency() {
-        fail("Test is not implemented yet.");
+        currencyConvertor.convert(Currency.getInstance(Locale.CHINA), Currency.getInstance(Locale.CANADA), new BigDecimal("10.00"));
     }
 
-    @Test
-    public void testConvertWithExternalServiceFailure() {
-        fail("Test is not implemented yet.");
+    @Test(expected = RuntimeException.class)
+    public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException {
+        when(exchangeRateTable.getExchangeRate(any(Currency.class), any(Currency.class))).thenThrow(ExternalServiceFailureException.class);
+        currencyConvertor.convert(Currency.getInstance("EUR"), Currency.getInstance("EUR"), new BigDecimal("1"));
     }
 
 }
