ae3d1eea2c4cb6f2945f1bf56e87d968d278db76
Tim3W4rp <martin@jordan.cz>
2017-10-11 12:37:41 +0200

    task01

--- eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task01.java
+++ eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task01.java
@@ -13,10 +13,9 @@ import cz.fi.muni.pa165.PersistenceSampleApplicationContext;
 import cz.fi.muni.pa165.entity.Category;
 
 
-
-public class Task01  {
-
-	
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+public class Task01 extends AbstractTestNGSpringContextTests{
+    
 	@PersistenceUnit
 	private EntityManagerFactory emf;
 
@@ -30,6 +29,13 @@ public class Task01  {
 		em.getTransaction().commit();
 		em.close();
 		//TODO under this line: create a second entity manager in categoryTest, use find method to find the category and assert its name.
+                
+                EntityManager em2 = emf.createEntityManager();
+                em2.getTransaction().begin();
+                Category newCat = em2.find(Category.class, cat.getId());
+                Assert.assertEquals(newCat.getName(), "Test");
+                em2.close();
+                
 	}
 
 }
